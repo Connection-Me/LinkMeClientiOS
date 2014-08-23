d57a8b954ba01ff8ac76a63062ecc746f822c5dd
ChaoSo <god_super@163.com>
2014-08-23 22:30:59 +0800

    bee framwork

---
+++ LinkMe/HAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HAppDelegate.h
+//  LinkMe
+//
+//  Created by Summer Wu on 14-8-23.
+//  Copyright (c) 2014年 Summer.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LinkMe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LinkMe
+//
+//  Created by Summer Wu on 14-8-23.
+//  Copyright (c) 2014年 Summer.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HAppDelegate class]));
+    }
+}
