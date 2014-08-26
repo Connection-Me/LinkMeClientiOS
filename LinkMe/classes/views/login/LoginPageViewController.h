80cb728bfe99784dd1f9212e770b17db38d6b522
ChaoSo <god_super@163.com>
2014-08-26 23:43:56 +0800

    update the Page

---
+++ LinkMe/classes/views/login/LoginPageViewController.h
@@ -0,0 +1,29 @@
+//
+//  ViewController.h
+//  CollapseClick
+//
+//  Created by Ben Gordon on 2/28/13.
+//  Copyright (c) 2013 Ben Gordon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CollapseClick.h"
+
+@interface LoginPageViewController : UIViewController <CollapseClickDelegate,UITextFieldDelegate> {
+    IBOutlet UIView *test1View;
+    IBOutlet UIView *test2View;
+    IBOutlet UIView *test3View;
+    
+    __weak IBOutlet CollapseClick *myCollapseClick;
+    
+    __weak IBOutlet UIImageView *galleryImage;
+}
+@property (weak, nonatomic) IBOutlet UITextField *loginNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *loginPwdTextField;
+@property (weak, nonatomic) IBOutlet UITextField *registerNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *registerPwdTextField;
+@property (weak, nonatomic) IBOutlet UITextField *refindNameTextField;
+@property (weak, nonatomic) IBOutlet UIButton *registerBtn;
+
+
+@end
