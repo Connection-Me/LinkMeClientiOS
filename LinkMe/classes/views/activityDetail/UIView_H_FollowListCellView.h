22b8bd35448ff359ae81a1c7fca8ae8f95342b50
ChaoSo <god_super@163.com>
2014-10-13 10:21:41 +0800

    the cell in HomeDetail Page

---
+++ LinkMe/classes/views/activityDetail/UIView_H_FollowListCellView.h
@@ -0,0 +1,13 @@
+//
+//  UIView_H_FollowListCellView.h
+//  LinkMe
+//
+//  Created by ChaoSo on 14-10-11.
+//  Copyright (c) 2014年 Summer.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView ()
+
+@end
